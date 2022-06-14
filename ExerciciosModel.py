d853a027244b80cc314cf337f294e373db148fb5
CristinAlvestegui <104037804+CristinAlvestegui@users.noreply.github.com>
2022-06-14 20:30:40 -0300

    Update ExerciciosModel.py

--- ExerciciosModel.py
+++ ExerciciosModel.py
@@ -25,11 +25,44 @@ def E4(dia, mes, ano):
     return msg
 
 def E5():
-    return 'são eleitorwes, pula'
+    return 'são eleitores, pula'
 
 def E6(sal):
     total = sal((sal * 10,16)/100)
     msg = 'Junto com a reajuste desse ano que foi de: 10.16%\n' + 'Fico um total de: {}'.format(total)
     return msg
+
+def E7():
+    return 'é carro, pula'
+
+def E8(nota1,nota2,nota3):
+    total = nota1 + nota2 + nota3
+    media = total / 3
+    msg = 'Sua nota média é de: {}'.format(media)
+    return msg
+
+def E9(maca):
+    if maca => 12:
+        total = maca
+        msg = 'Obrigada pela compra!!\n\n' + 'Você comprou: {}maças, com um valor total de {}R$, cada uma\n'.format(maca, total)
+        return msg
+    else:
+        total = int(maca * 1,30)
+        msg = 'Obrigada pela compra!!\n\n' + 'Você comprou: {}maças, com um valor total de {}R$, cada uma\n'.format(maca, total)
+        return msg
+def E10():
+    return 'que preguiçaaaaaaaa'
+def E13(num1):
+    if num1 < 10:
+        num2 = 0
+        multi = num1 * num2
+        for num2 in range(10):
+            msg = '{} X {} = {}'.format(num1, num2, multi)
+        return msg
+    else:
+        return 'Por favor informe um número entre 1 e 10'
+    
+    
+    
     
     
