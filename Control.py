b198be24a7d910f439bcc49ab6313a00609ba8a0
Cristina Alvéstegui <104037804+CristinAlvestegui@users.noreply.github.com>
2022-06-18 17:27:01 -0300

    Update Control.py

--- Control.py
+++ Control.py
@@ -42,26 +42,40 @@ def PegaAlura():
 def PegaSal():
     print('Por favor informe seu salario mensal')
     this.sal = float(input())
+    
+def PegaVenda():
+    PegaSal()
+    print('Digite o valor das vendas de hoje')
+    this.venda = float(input())
 
 def PegaFabri():
     print('Informe o custo da Fábrica')
     this.fabri = float(input())
     
-def PegaNota1():
+def PegaNotas():
     print('Informe a primeira nota:')
     this.nota1 = float(input())
-    
-def PegaNota2():
     print('Informe a segunda nota:')
     this.nota2 = float(input())
- 
-def PegaNota3():
     print('Informe a terceira nota:')
-    this.nota2 = float(input())
+    this.nota2 = float(input())   
 
 def PegaMaca():
     print('Por favor informe quantas maças você vai comprar:')
     this.maca = float(input())
+   
+def PegarElei():
+    print('Digite o número total de eleitores do Municipio')
+    this.elei = float(input())
+    
+def PegaVoto():
+    PegarElei();
+    print('Informe o número de votos válidos')
+    this.votoV = float(input())
+    print('Informe o número de votos nulos')
+    this.votoN = float(input())
+    print('Informe o número de votos em branco')
+    this.votoB = float(input())
 
 def Executar():
     while(this.opcao != 0):
@@ -82,15 +96,16 @@ def Executar():
             PegaM()
             PegaA()
             print(ExerciciosModel.E4(this.dia, this.mes, this.ano))
+        elif this.opcao == 5:
+            PegaVoto()
+            print(ExerciciosModel.E5(this.elei, this.votoV, this.votoN, this.votoB))
         elif this.opcao == 6:
             print(ExerciciosModel.E6(sal))
         elif this.opcao == 7:
             PegaFabri()
             print(ExercicioModel.E7(this.fabri))
         elif this.opcao == 8:
-            PegaNota1()
-            PegaNota2()
-            PegaNota3()
+            PegaNotas()            
             print(ExercicioModel.E8(this.nota1, this.nota2, this.nota3))
         elif this.opcao == 9:
             PegaMaca()
@@ -98,7 +113,8 @@ def Executar():
         elif this.opcao == 10:
             print('')
         elif this.opcao == 11:
-            print('')
+            PegaVenda()   
+            print(ExercicioModel.E11(thi.sal, this.venda))
         elif this.opcao == 12:
             print('')
         elif this.opcao == 13:
